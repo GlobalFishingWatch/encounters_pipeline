8b029cd86d5732ba3db1d82d2bc0879636ba4e28
bitsofbits <tim@bitsofbits.com>
2017-11-03 14:37:25 -0700

    Fix to allow to work on more than 1000 days

--- pipeline/raw_definition.py
+++ pipeline/raw_definition.py
@@ -1,4 +1,5 @@
 from apache_beam import io
+from apache_beam import Flatten
 from pipeline.objects.record import RecordsFromDicts
 from pipeline.transforms.group_by_id import GroupById
 from pipeline.transforms.sort_by_time import SortByTime
@@ -19,10 +20,13 @@ class RawPipelineDefinition():
             write_disposition=self.options.raw_sink_write_disposition,
         )
 
+        sources = [(pipeline | "Read_{}".format(i) >> io.Read(io.gcp.bigquery.BigQuerySource(query=x)))
+                        for (i, x) in enumerate(self.options.source.split('\n\n')) if x.strip()]
+
 
         (
-            pipeline
-            | io.Read(io.gcp.bigquery.BigQuerySource(query=self.options.source))
+            sources
+            | Flatten()
             | RecordsFromDicts()
             | Resample(increment_s = 60 * 10, max_gap_s = 60 * 60 * 1) # TODO: parameterize
             | ComputeAdjacency(max_adjacency_distance_km=1.0) # TOD: parameterize
