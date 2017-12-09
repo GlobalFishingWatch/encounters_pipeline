df04069626eb533400097fb42956e65f2a1b8a80
bitsofbits <tim@bitsofbits.com>
2017-12-09 12:35:24 -0700

    ad GCS_BUCKET to post_install

--- post_install.sh
+++ post_install.sh
@@ -2,6 +2,7 @@ python $AIRFLOW_HOME/utils/set_default_variables.py PIPE_ENCOUNTERS \
     SOURCE_TABLE=pipeline_classify_p_p516_daily \
     RAW_TABLE=world-fishing-827:machine_learning_dev_ttl_30d.raw_encounters_test \
     SINK_TABLE=world-fishing-827:machine_learning_dev_ttl_30d.encounters_test \
+    GCS_BUCKET=machine-learning-dev-ttl-30d/encounters \
     DOCKER_IMAGE=gcr.io/world-fishing-827/$1
 
 echo "Installation Complete"
