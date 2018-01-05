312f304de08f412f98c59a8694d4279f1bba24c9
bitsofbits <tim@bitsofbits.com>
2018-01-05 06:48:57 -0700

    tweak default vars

--- post_install.sh
+++ post_install.sh
@@ -2,8 +2,8 @@ python $AIRFLOW_HOME/utils/set_default_variables.py \
     --force DOCKER_IMAGE=$1 \
     PIPE_ENCOUNTERS \
     SOURCE_TABLE=pipeline_classify_p_p516_daily \
-    RAW_TABLE=world-fishing-827:machine_learning_dev_ttl_30d.raw_encounters_test \
-    SINK_TABLE=world-fishing-827:machine_learning_dev_ttl_30d.encounters_test \
+    RAW_TABLE=world-fishing-827:machine_learning_dev_ttl_30d.raw_encounters_ \
+    SINK_TABLE=world-fishing-827:machine_learning_dev_ttl_30d.encounters_ \
     GCS_BUCKET=machine-learning-dev-ttl-30d/encounters
 
 echo "Installation Complete"
