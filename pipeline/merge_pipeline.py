8468e80ff9e895c7bfe8808b5bc4ed1d9f5c68f8
Matias Piano <matias@globalfishingwatch.org>
2020-01-15 11:02:07 +0100

    PTransform Encounters.toDict handle as dict not as obj

--- pipeline/merge_pipeline.py
+++ pipeline/merge_pipeline.py
@@ -21,6 +21,12 @@ import six
 def ensure_bytes_vessel_id(obj):
     return obj._replace(vessel_1_id=six.ensure_binary(obj.vessel_1_id))._replace(vessel_2_id=six.ensure_binary(obj.vessel_2_id))
 
+def ensure_bytes_vessel_id_dict(dictionary):
+    logging.info('ensure_bytes_vessel_id_dict=%s' % dictionary)
+    dictionary['vessel_1_id']=six.ensure_binary(dictionary['vessel_1_id'])
+    dictionary['vessel_2_id']=six.ensure_binary(dictionary['vessel_2_id'])
+    return dictionary
+
 def run(options):
 
     p = Pipeline(options=options)
@@ -51,7 +57,7 @@ def run(options):
     raw_encounters = (sources
         | Flatten()
         | Encounter.FromDict()
-        | 'Ensure ID is bytes' >> Map(ensure_bytes_vessel_id)
+        | 'Ensure VESSEL_X_ID is bytes' >> Map(ensure_bytes_vessel_id)
     )
 
     if merge_options.min_encounter_time_minutes is not None:
@@ -73,7 +79,7 @@ def run(options):
         | FilterPorts()
         | FilterInland()
         | "FilteredToDicts" >> Encounter.ToDict()
-        | 'Ensure ID is bytes' >> Map(ensure_bytes_vessel_id)
+        | 'Ensure VESSEL_X_ID is bytes' >> Map(ensure_bytes_vessel_id_dict)
         | "WriteFiltered" >> writer_filtered
     )
 
