9c0e6c268465bf153f0e0353a0c9d8ae79ed15af
Matias Piano <matias@globalfishingwatch.org>
2020-09-18 16:20:38 -0300

    Increments version and document stabilization of encounters merge in CHANGES.md

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -4,7 +4,7 @@ Apache Beam pipeline for computing vessel encounters.
 
 
 
-__version__ = '3.0.3'
+__version__ = '3.0.4'
 __author__ = 'Global Fishing Watch'
 __email__ = 'info@globalfishingwatch.org'
 __source__ = 'https://github.com/GlobalFishingWatch/anchorages_pipeline'
