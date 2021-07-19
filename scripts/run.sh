cdb961689ecf23a732b17cfdf8043c1813e84528
Matias Piano <matias@globalfishingwatch.org>
2021-07-19 02:44:19 -0300

    Automate the encounters with seg-id

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,9 +3,9 @@
 THIS_SCRIPT_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"
 
 display_usage() {
-	echo "Available Commands"
-	echo "  create_raw_encounters       create raw encounters dataflow"
-	echo "  merge_encounters            merge encounters dataflow"
+  echo "Available Commands"
+  echo "  create_raw_encounters       Create raw encounters dataflow"
+  echo "  merge_encounters            Merge encounters dataflow"
 }
 
 
@@ -19,15 +19,12 @@ fi
 case $1 in
 
   create_raw_encounters)
-
     python -m pipeline.create_raw_encounters "${@:2}"
     ;;
 
   merge_encounters)
-
     python -m pipeline.merge_encounters "${@:2}"
     ;;
-
 
   *)
     display_usage
