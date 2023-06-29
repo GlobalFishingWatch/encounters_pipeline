712120ac926e31350b1b4f350177f599240d1252
Matias Piano <matias@globalfishingwatch.org>
2023-06-29 01:49:32 -0300

    Removes the lib ujson that was not used and create a WriteEncountersToBQ class

--- pipeline/transforms/writers.py
+++ pipeline/transforms/writers.py
@@ -1,22 +1,39 @@
-from apache_beam import PTransform
 from apache_beam import io
 from pipeline.schemas.output import build as output_schema
-import ujson as json
+from pipeline.utils.ver import get_pipe_ver
 
+table_description = lambda write_opts: f"""
+    Created by the encounters_pipeline: {get_pipe_ver()}.
+    * Merges the encounters that are close in time into one long encounter.
+    * https://github.com/GlobalFishingWatch/encounters_pipeline
+    * Source: {write_opts.source_table}
+    * Maximum distance for vessels to be elegible (km): {write_opts.max_encounter_dist_km}
+    * Minimum minutes of vessel adjacency before we have an encounter: {write_opts.min_encounter_time_minutes}
+    * Date range: {write_opts.start_date.strftime('%Y-%m-%d')},{write_opts.end_date.strftime('%Y-%m-%d')}
+"""
 
-class WriteToBq(PTransform):
-    def __init__(self, table=None, write_disposition=None):
-        self.table = table
-        self.write_disposition = write_disposition
+class WriteEncountersToBQ(io.WriteToBigQuery):
 
-    def expand(self, xs):
-        big_query_sink = io.gcp.bigquery.BigQuerySink(
-            table=self.table,
-            write_disposition=self.write_disposition,
+    def __init__(self, options:dict):
+        super(io.WriteToBigQuery, self).__init__(
+            options.sink_table,
+            project=options.project,
             schema=output_schema(),
+            write_disposition=io.BigQueryDisposition.WRITE_APPEND,
+            create_disposition=io.BigQueryDisposition.CREATE_IF_NEEDED,
+            additional_bq_parameters={
+                "timePartitioning": {
+                    "type": "MONTH",
+                    "field": "start_time",
+                    "requirePartitionFilter": True
+                },
+                "clustering": {
+                    "fields": ["start_time"]
+                },
+                "destinationTableProperties": {
+                    "description": table_desc(options),
+                },
+            }
         )
 
-        return (
-            xs
-            | io.Write(big_query_sink)
-        )
+
