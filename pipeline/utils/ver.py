017f06b0258cefdfd31f83f01ec8db59a02410d8
Matias Piano <matias@globalfishingwatch.org>
2023-06-30 16:53:02 -0300

    Adds PipelineOptions to each TestPipeline to make tests work

--- pipeline/utils/ver.py
+++ pipeline/utils/ver.py
@@ -3,4 +3,4 @@ import re
 def get_pipe_ver():
     """Returns the version of the package."""
     with open('setup.py') as rf:
-        return re.search(r'version=\'([0-9.]*)\'', rf.read()).group(1)
+        return re.search(r"version='([0-9.]*)'", rf.read()).group(1)
